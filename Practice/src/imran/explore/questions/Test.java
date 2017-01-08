4ea27a11c35412c6ae00ab6eea25dc5c84b7afc2
ImranO <ImranO@ImranO>
2017-01-08 14:15:13 +0530

    commit

---
+++ Practice/src/imran/explore/questions/Test.java
@@ -0,0 +1,13 @@
+package imran.explore.questions;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		String s= in.next();
+		System.out.println("Testing! "+s);
+	}
+
+}
